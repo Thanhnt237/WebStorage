a7d4311dd288e1335bb8f0375c62bf77c768b3d5
Thanhnt237 <tuanthanh9a22014@gmail.com>
2021-08-18 10:04:02 +0700

    update routes

--- routes/core.server.routes.js
+++ routes/core.server.routes.js
@@ -9,11 +9,11 @@ var router = express.Router();
 
 const coreCtrl = require('../controllers').Core;
 
-  router.route('/').get(coreCtrl.renderHomePage);
-  router.route('/add-product').get(coreCtrl.renderAddProductPage);
-  router.route('/update-product/:productId').get(coreCtrl.renderUpdateProductPage);
-  router.route('/').post(coreCtrl.AddNewProduct);
-  router.route('/:productId').delete(coreCtrl.DeleteProduct);
-  router.route('/:productId').post(coreCtrl.UpdateProduct);
+  router.route('/admin').get(coreCtrl.renderHomePage);
+  router.route('/admin/add-product').get(coreCtrl.renderAddProductPage);
+  router.route('/admin/update-product/:productId').get(coreCtrl.renderUpdateProductPage);
+  router.route('/admin').post(coreCtrl.AddNewProduct);
+  router.route('/admin/:productId').delete(coreCtrl.DeleteProduct);
+  router.route('/admin/:productId').post(coreCtrl.UpdateProduct);
 
 module.exports = router;
